3e9c01142da2e9611ac25feee568584224310abf
Stefan Stax <stefanstaxbusiness@gmail.com>
2023-07-04 11:08:39 +0200

    Components updated to support filtering | Design updates | slices updates with combined reducer

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,8 +3,15 @@ import { removeCar } from "../store/slices";
 
 const CarList = () => {
   const dispatch = useDispatch();
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const { cars, name } = useSelector(({ form, cars: { data, searchTerm } }) => {
+    const filteredCars = data.filter((car) =>
+      car?.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = (carId) => {
@@ -12,8 +19,9 @@ const CarList = () => {
   };
 
   const displayCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
     return (
-      <div key={car?.id} className="panel">
+      <div key={car?.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car?.name || "Name wasn't provided"} - ${car?.cost || "0"}
         </p>
